fd8146b684864c87b95c1d439d9bdb8134fe8e08
BrewToFew <stefanbelleflame@gmail.com>
2025-05-11 14:01:42 +0200

    2025-05-11

---
+++ hello_world/CMakeFiles/hello_world.self-self.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_world.self-self"
+  "hello_world.self"
+  "hello_world.self.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/hello_world.self-self.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
